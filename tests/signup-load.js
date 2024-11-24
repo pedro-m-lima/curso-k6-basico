41ec13d1593bbbe90f4e19ad92f3b1b24fe3a561
pedro-m-lima <pedromlima.pm@gmail.com>
2024-11-24 13:30:06 -0300

    Finalizacao da aula teste de carga
    Criado um Load teste onde configurei stages que seriam passados durante a execucao, garantindo que o sistema se comportara de acordo com o esperado mesmo com picos de carga

---
+++ tests/signup-load.js
@@ -0,0 +1,40 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import uuid from './libs./uuid.js';
+
+//No teste de carga conseguimos definir picos de requisições dentro de um determinado stagio
+export const options = { 
+  stages: [
+    {duration: '1m', target: 100},
+    {duration: '2m', target: 100},
+    {duration: '1m', target: 0},
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<2000'], 
+    http_req_failed: ['rate<0.01'] 
+  }
+};
+export default function () {
+
+  const url = "http://localhost:3333/signup"
+
+  const payload = JSON.stringify( 
+    { email: `${uuid.v4().substring(24)}@pedrok6.com`, password: 'pwd123' }
+  )
+  
+  const headers = { 
+    'headers': {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const res = http.post(url, payload, headers); 
+
+  console.log(res.body) 
+
+  check(res, { 
+
+  })
+
+  sleep(1); 
+}
