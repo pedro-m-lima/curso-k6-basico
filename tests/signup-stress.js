407730f45dc371592ddd9ae6c36fe6a1d2f6c69a
pedro-m-lima <pedromlima.pm@gmail.com>
2024-11-24 13:57:32 -0300

    Finalizacao aula testes de estresse
    Criado um script que ao executar extrapola os limites funcionais especificado, com isso coloca o sistema em uma condição extrema stress podendo garantir que caso aconteça determinada situação o sistema ainda suporta, e verificar os niveis ao qual o sistema vai se comportar

--- tests/signup-stress.js
+++ tests/signup-stress.js
@@ -5,9 +5,13 @@ import uuid from './libs./uuid.js';
 //No teste de carga conseguimos definir picos de requisições dentro de um determinado stagio
 export const options = { 
   stages: [
-    {duration: '1m', target: 100},
     {duration: '2m', target: 100},
-    {duration: '1m', target: 0},
+    {duration: '5m', target: 100},
+    {duration: '2m', target: 200},
+    {duration: '5m', target: 300},
+    {duration: '2m', target: 400},
+    {duration: '5m', target: 400},
+    {duration: '10m', target: 0},
   ],
   thresholds: {
     http_req_duration: ['p(95)<2000'], 
