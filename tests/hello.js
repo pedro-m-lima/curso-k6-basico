fe96c2e111bfdc721cdfe6f0485a04e5c550e865
pedro-m-lima <pedromlima.pm@gmail.com>
2024-11-24 08:22:39 -0300

    Atualização cenário Test.sh e modificações no hello.js

---
+++ tests/hello.js
@@ -0,0 +1,10 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+export const options = {
+  //vus: 1,
+  //duration: '30s',
+};
+export default function () {
+  http.get('http://localhost:3333');
+  sleep(1);
+}
