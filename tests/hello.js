bbe8af9ac8858822eb97cf3fd8dde90492010645
pedro-m-lima <pedromlima.pm@gmail.com>
2024-11-24 08:44:42 -0300

    Finalização aula validacao check status code

--- tests/hello.js
+++ tests/hello.js
@@ -1,10 +1,17 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check} from 'k6'; //Importação de funções
 export const options = {
   //vus: 1,
   //duration: '30s',
 };
 export default function () {
-  http.get('http://localhost:3333');
-  sleep(1);
+  const res = http.get('http://localhost:3333'); //Atribuindo a requisição get a constante res
+  
+  //assert
+  check(res, { //definindo um bloco que valida o resultado de "res"
+        'status code deve ser 200': (r) => r.status === 200 //Aqui adicionamos um resultado esperado onde, (r) recebe o resultado da requisição res e valida se o status é igual a 200
+
+  })
+
+  sleep(1); //Utilizando o sleep adicionamos uma pausa de 1 segundo que seria a simulação de um usuário pensando.
 }
