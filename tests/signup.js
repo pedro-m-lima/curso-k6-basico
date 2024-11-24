e563e7e85e75eec64eaadf6d7f0f2d84959f46bd
pedro-m-lima <pedromlima.pm@gmail.com>
2024-11-24 11:39:51 -0300

    Finalizacao da aula massa de testes dinamicas com UUID
    Nessa aula utilizando uma biblioteca node chama uuid onde conseguimos gerar ids dinamicos, com isso adptamos para que conseguir gerar uma massa de testes dinamica onde a cada requisição um usuário recebia um id unica fazendo com que nao barrasse em uma regra de negocio a qual nao permitia incluir usuario duplicados. Por o uuid ser uma biblioteca node, foi necessario converter essa biblioteca node pura para java script, e para isso utilizar o browserify para converter de node puro para um arquivo.js com isso conseguimos rodar a biblioteca no ambiente local sem necessitar de um url externa podendo gerar gargalo na conexao e fazendo com que nossos testes não ficassem fieis pois iria depender de conexoes externas

--- tests/signup.js
+++ tests/signup.js
@@ -1,5 +1,7 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+import uuid from './libs./uuid.js';
+
 export const options = {
   //vus: 1,
   //duration: '30s',
@@ -9,7 +11,7 @@ export default function () {
   const url = "http://localhost:3333/signup"
 
   const payload = JSON.stringify( //JSON é um modulo nativo do JavaScript e stringify é uma função que Transforma um objeto jason em uma string mantendo seu formado
-    { email: 'papito@yahoo.com', password: 'pwd123' }
+    { email: `${uuid.v4().substring(24)}@pedrok6.com`, password: 'pwd123' } //nome email abrimos uma interpolação de dados e chamamos a função que gera um identificador a cada usuario através da função.v4
   )
   
   //Objeto que define o cabeçalho da requisição
@@ -26,7 +28,7 @@ export default function () {
 
   //assert
   check(res, { //definindo um bloco que valida o resultado de "res"
-    'status code deve ser 201': (r) => r.status === 201 //Aqui adicionamos um resultado esperado onde, (r) recebe o resultado da requisição res e valida se o status é igual a 200
+    'status code deve ser 201': (r) => r.status === 201 //Aqui adicionamos um resultado esperado onde, (r) recebe o resultado da requisição res e valida se o status é igual a 201
 
   })
 
